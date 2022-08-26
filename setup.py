af597b975b98358125787463798b3a414cf90174
Ibrohim Bultakov <bii23.uz@gmail.com>
2022-08-26 17:07:42 +0500

    Xatolar tuzatildi!!!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='aiopay',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/bultakov/aiopay',
     license='MIT',
     author='Ibrohim Bultakov',
